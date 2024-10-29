646aaae42c76f724547ddd6958555f8cec84cf21
Mohammed El Bachiri <mohammed-el-bachiri@users.noreply.github.com>
2024-10-29 15:29:12 +0100

    project setup

---
+++ upskillers/src/test/java/com/upskillers/upskillers/UpSkillersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upskillers.upskillers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpSkillersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
